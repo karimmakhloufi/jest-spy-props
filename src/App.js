bca3b92c627d202bb9950247d9d14fb9194716ef
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-05-28 16:47:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function App({ handleClick }) {
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <button onClick={handleClick}></button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  const handleClickFunction = jest.fn();
+  render(<App handleClick={handleClickFunction} />);
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+  userEvent.click(button);
+  expect(handleClickFunction).toHaveBeenCalledTimes(2);
 });
